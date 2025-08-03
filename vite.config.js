9f74055e14c12b8bc1b63567696a5470adf2bf53
Pratham-000 <prathamarora925@gmail.com>
2025-08-03 11:02:41 +0530

    Setup GitHub Pages Deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: "/Job-Portal-Application-/", // <-- this should match your repo name
   plugins: [ tailwindcss(),react()],
 })
